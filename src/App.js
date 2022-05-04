4361f9b3c5c92b8cf1e42d9b9c4ad1d76b92d36f
Erxical <erxinxiao@gmail.com>
2022-05-04 15:41:09 +0700

    W2-04 React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import { Component } from 'react';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <Main />
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+
       </div>
     );
   }
